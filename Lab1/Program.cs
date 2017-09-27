1fe4d4a019ab1ae7e854545a4dcbbd84dc0816eb
iaguilar313 <iaguilar@umich.edu>
2017-09-27 10:27:33 -0400

    This is the first commit!

---
+++ Lab1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ProgramContinue = true;
+            while (ProgramContinue == true)
+            {
+
+                Double Lenght;
+                Console.WriteLine("Enter lenght of room ");
+                Lenght = double.Parse(Console.ReadLine());
+
+                Double Width;
+                Console.WriteLine("Enter width of room");
+                Width = double.Parse(Console.ReadLine());
+
+                Double Area;
+                Area = Lenght * Width;
+                Console.WriteLine("The Area is");
+                Console.WriteLine(Area);
+
+
+                Double Perimeter;
+                Perimeter = (Lenght * 2) + (Width * 2);
+                Console.WriteLine("The Perimeter is");
+                Console.WriteLine(Perimeter);
+
+
+                Console.WriteLine("Do you want to continue? Enter Y or N");
+                string UserInput;
+                UserInput = Console.ReadLine();
+
+                if (UserInput == "N")
+
+                {
+                    ProgramContinue = false;
+                }
+
+
+
+
+
+
+
+
+
+            }
+        }
+
+    }
+}
